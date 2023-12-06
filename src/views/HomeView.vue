c4c16430a948aa0926bcb4b9f5271c2eb5c68157
xieshuang <hi.xieshuang@gmail.com>
2023-12-06 16:58:16 +0800

    add svg component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,9 @@
         <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
         <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
         <el-button @click="openDialog">Open Dialog</el-button>
+        <div>
+            <IconUser style="width: 80px; height: 80px; color: #2ad" />
+        </div>
     </div>
 </template>
 
@@ -11,10 +14,12 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 import HelloDialog from '@/components/HelloDialog.vue';
 import { dialogAPI } from '@/common/dialogAPI';
+import IconUser from '@/components/icons/IconUser.vue';
 
 export default {
     name: 'HomeView',
     components: {
+        IconUser
         // HelloWorld
     },
     async mounted() {
