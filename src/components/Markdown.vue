d91e4556fccb4f9404b9342ac4c06f9c049ef396
xieshuang <hi.xieshuang@gmail.com>
2024-03-12 10:58:05 +0800

    add markdown component

---
+++ src/components/Markdown.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="markdown-body">
+        <slot />
+    </div>
+</template>
+
+<script>
+import 'github-markdown-css';
+
+export default {};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,23 @@
         <div>
             <IconUser style="width: 80px; height: 80px; color: #2ad" />
         </div>
+        <br />
+        <br />
+        <Markdown>
+            <h4>工具使用说明</h4>
+            <ul>
+                <li>用途：配之后，所附近的考拉附近的萨房间都开了房间里盛开的积分多少</li>
+                <li>
+                    <div>配置说明：</div>
+                    <ul>
+                        <li>应用名称：疯狂的萨减肥撒加分</li>
+                        <li>是否开启：附近的快速反击的萨</li>
+                        <li>分支ID：附近考虑的萨减肥了肯定是减肥</li>
+                    </ul>
+                </li>
+                <li>PS：附近看到啦附近的路上就发了多少减肥看了电视剧啊发；了萨过年了萨发</li>
+            </ul>
+        </Markdown>
     </div>
 </template>
 
@@ -15,11 +32,13 @@
 import HelloDialog from '@/components/HelloDialog.vue';
 import { dialogAPI } from '@/common/dialogAPI';
 import IconUser from '@/components/icons/IconUser.vue';
+import Markdown from '@/components/Markdown.vue';
 
 export default {
     name: 'HomeView',
     components: {
-        IconUser
+        IconUser,
+        Markdown
         // HelloWorld
     },
     async mounted() {
